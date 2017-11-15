dd86d208746ff206407641e1f4cbd9b0d6ce2e18
杨鸿康 <AdminYHK@DESKTOP-RKA7V5H>
2017-11-15 09:41:07 +0800

    init

---
+++ AdminYhk-Boot-Cloud/AdminYhk-Eureka/src/main/java/org/AdminYhk/Eureka/App.java
@@ -0,0 +1,13 @@
+package org.AdminYhk.Eureka;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
